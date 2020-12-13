2a837e64edb59916047d216201498d097426c30b
Sumit <sumitsaurabh9868@gmail.com>
2020-12-13 13:29:04 +0530

    implemented peek, isEmpty and isFull in stack

--- cpp/stack/stackArray.cpp
+++ cpp/stack/stackArray.cpp
@@ -7,23 +7,28 @@ private:
     T *stk;
     int top;
     int cap;
+    int size;
 
 public:
+    stackArray()
+    {
+    }
     stackArray(int c)
     {
         this->cap = c;
+        this->size = 0;
         this->stk = new T[c];
         this->top = -1;
     }
     void push(T item)
     {
-        if (this->cap = 0)
+        if (this->size == this->cap)
         {
-            cout << "Overflow\n";
+            cout <<"cannot add "<<item<< "...Overflow\n";
             return;
         }
         this->top++;
-        this->cap--;
+        this->size++;
         stk[top] = item;
     }
     T pop()
@@ -35,30 +40,53 @@ public:
             return item;
         }
 
-        item=stk[this->top];
+        item = stk[this->top];
         this->top--;
-        this->cap++;
+        this->size--;
+        cout << "popping the queue ";
         return item;
     }
 
-    void display(){
-        cout<<"current statck is\n";
-        for (int i = this->top; i >=0; i--)
+    void display()
+    {
+        cout << "current statck is\n";
+        for (int i = this->top; i >= 0; i--)
         {
-            cout<<this->stk[i];
-            if(i==this->top){
-                cout<<"<-top";
+            cout << this->stk[i];
+            if (i == this->top)
+            {
+                cout << "<-top";
             }
-            cout<<endl;
+            cout << endl;
         }
-            cout<<"---------------------"<<endl;
-        
+        cout << "---------------------" << endl;
     }
 
+    T peek()
+    {
+        cout << "the queue top is";
+        T item;
+        item = stk[top];
+        return item;
+    }
+
+    bool isEmpty()
+    {
+        return this->size == 0;
+    }
+
+    bool isFull()
+    {
+        return this->cap == this->size;
+    }
 };
 int main()
 {
     stackArray<int> stk1(10);
+    if (stk1.isEmpty())
+    {
+        cout << "the queue is empty now\n";
+    }
     stk1.push(1);
     stk1.push(5);
     stk1.push(6);
@@ -66,8 +94,17 @@ int main()
     stk1.push(81);
     stk1.push(94);
     stk1.push(54);
+    stk1.push(64);
+    stk1.push(12);
+    stk1.push(543);
     stk1.display();
-    cout<<stk1.pop()<<endl;
+    if (stk1.isFull())
+    {
+        cout << "the queue is full now\n";
+    }
+    cout << stk1.pop() << endl;
+    cout<<stk1.peek()<<endl;
     stk1.display();
+
     return 0;
 }
--- cpp/stack/stackList.cpp
+++ cpp/stack/stackList.cpp
@@ -5,53 +5,86 @@ class stackList
 {
     class node
     {
-        
+
     public:
         T data;
-            node *next;
-        node(T t){
-            this->data=t;
+        node *next;
+        node(T t)
+        {
+            this->data = t;
+            this->next = nullptr;
+        }
+        node()
+        {
             this->next=nullptr;
         }
-        node(){
-
-        }
-        
     };
-    
+
     node *top;
-    public:
-    stackList(){
-        top=nullptr;
+    int cap;
+    int size;
+
+public:
+    stackList()
+    {
+        this->top = nullptr;
     }
-    void push(T t){
-        node *nn=new node();
-        nn->data = t;
-        nn->next = nullptr;
-        nn->next =top;
-        top=nn;
+    stackList(int c)
+    {
+        this->cap = c;
+        this->size = 0;
+        this->top = nullptr;
+    }
+    void push(T t)
+    {
+        node *nn = new node(t);
+        nn->next = this->top;
+        this->top = nn;
+        this->size++;
     }
 
-    T pop(){
-        T item =top->data;
-        top=top->next;
+    T pop()
+    {
+        T item = this->top->data;
+        this->top = this->top->next;
+        this->size--;
         return item;
     }
 
-    void display(){
+    void display()
+    {
         node *t = this->top;
-        cout<<"top->";
-        while (t != nullptr)
+        while (t!= nullptr)
         {
-            cout << t->data<<"\n";
+            cout << t->data;
+            if (t == this->top)
+            {
+                cout << "<-top";
+            }
+            cout << endl;
             t = t->next;
         }
     }
-    
+    T peek()
+    {
+        T item = this->top->data;
+        return item;
+    }
+
+    bool isEmpty()
+    {
+        return this->size == 0;
+    }
+
+    bool isFull()
+    {
+        return this->size == this->cap;
+    }
 };
 int main()
 {
     stackList<int> stk;
+    stk.display();
     stk.push(10);
     stk.push(30);
     stk.push(50);
@@ -60,7 +93,8 @@ int main()
     stk.push(16);
     stk.push(13);
     stk.display();
-    cout<<endl<<stk.pop()<<endl;
+    cout << endl;
+    cout<< stk.pop() << endl;
     stk.display();
     return 0;
 }
